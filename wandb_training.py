a3b441f27f536d2cd21e22078e20538cfb2b8f2f
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-01-13 11:51:00 +0100

    wandb init

--- wandb_training.py
+++ wandb_training.py
@@ -12,43 +12,44 @@ dotenv.load_dotenv()
 device = 'cuda'
 
 
-def train():
+def train(config=None):
     global train_ds, val_ds
-    config = wandb.config
-    train_loader = DataLoader(train_ds, batch_size=config.batch_size, shuffle=True, pin_memory=True)
-    val_loader = DataLoader(val_ds, batch_size=config.batch_size, shuffle=False)
+    with wandb.init(config=config, group='SimpleNet') as run:
+        config = wandb.config
+        train_loader = DataLoader(train_ds, batch_size=config.batch_size, shuffle=True, pin_memory=True)
+        val_loader = DataLoader(val_ds, batch_size=config.batch_size, shuffle=False)
 
-    backbone = backbones.load(config.backbone.backbone_name)
-    net = SimpleNet(device)
-    net.load(
-        backbone=backbone,
-        layers_to_extract_from=config.backbone.backbone_layers,
-        device=device,
-        input_shape=config.input_shape,
-        pretrain_embed_dimension=config.pretrain_embed_dimension,
-        target_embed_dimension=config.projection_dimension,
-        patchsize=config.patch_size,
-        embedding_size=None,
-        meta_epochs=config.meta_epochs,
-        aed_meta_epochs=config.aed_meta_epochs,
-        gan_epochs=config.gan_epochs,
-        noise_std=config.noise_std,
-        dsc_layers=config.dsc_layers,
-        dsc_hidden=config.dsc_hidden,
-        dsc_margin=config.dsc_margin,
-        dsc_lr=config.lr,
-        auto_noise=config.auto_noise,
-        train_backbone=config.train_backbone,
-        cos_lr=config.cos_lr,
-        pre_proj=config.pre_proj,
-        proj_layer_type=config.proj_layer_type,
-        mix_noise=config.mix_noise,
-    )
-    models_dir = f"models/{time()}"
-    dataset_name = "rsna_breast_cancer"
-    net.set_model_dir(models_dir, dataset_name)
+        backbone = backbones.load(config.backbone.backbone_name)
+        net = SimpleNet(device, wandb_run=run)
+        net.load(
+            backbone=backbone,
+            layers_to_extract_from=config.backbone.backbone_layers,
+            device=device,
+            input_shape=config.input_shape,
+            pretrain_embed_dimension=config.pretrain_embed_dimension,
+            target_embed_dimension=config.projection_dimension,
+            patchsize=config.patch_size,
+            embedding_size=None,
+            meta_epochs=config.meta_epochs,
+            aed_meta_epochs=config.aed_meta_epochs,
+            gan_epochs=config.gan_epochs,
+            noise_std=config.noise_std,
+            dsc_layers=config.dsc_layers,
+            dsc_hidden=config.dsc_hidden,
+            dsc_margin=config.dsc_margin,
+            dsc_lr=config.lr,
+            auto_noise=config.auto_noise,
+            train_backbone=config.train_backbone,
+            cos_lr=config.cos_lr,
+            pre_proj=config.pre_proj,
+            proj_layer_type=config.proj_layer_type,
+            mix_noise=config.mix_noise,
+        )
+        models_dir = f"models/{time()}"
+        dataset_name = "rsna_breast_cancer"
+        net.set_model_dir(models_dir, dataset_name)
 
-    net.train(train_loader, val_loader)
+        net.train(train_loader, val_loader)
 
 
 img_dir = os.environ['IMAGE_DIR']
