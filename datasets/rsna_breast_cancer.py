161c0ab63466ea8037929df2cb99245066e876b2
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-01-13 12:19:47 +0100

    ds

--- datasets/rsna_breast_cancer.py
+++ datasets/rsna_breast_cancer.py
@@ -96,8 +96,8 @@ class BreastCancerDataset(Dataset):
         return len(self.metaData)
 
     def __getitem__(self, idx):
-        patient_id = self.metaData.iloc[idx, 1]
-        image_id = self.metaData.iloc[idx, 2]
+        patient_id = str(self.metaData.iloc[idx, 1])
+        image_id = str(self.metaData.iloc[idx, 2])
         img_name = f"{image_id}.png"
         img_path = os.path.join(self.img_dir, patient_id, img_name)
         image = Image.open(img_path)
