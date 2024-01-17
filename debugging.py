3154a07e9fba85b52e22f4e9b8598ef81e66ebc7
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-01-17 14:39:41 +0100

    debug

--- debugging.py
+++ debugging.py
@@ -15,18 +15,26 @@ csv_file = os.environ['CSV_PATH']
 train_ds = BreastCancerDataset(
     img_dir=img_dir,
     meta_data_csv_path=csv_file,
-    num_images=(10000, 0, 768, 0)
+    num_images=(128, 0, 128, 0)
 )
 
 val_ds = BreastCancerDataset(
     img_dir=img_dir,
     meta_data_csv_path=csv_file,
     split=DatasetSplit.VAL,
-    num_images=(1000, 10000, 1158-768, 768)
+    num_images=(128, 0, 128, 0)
 )
 
-train_loader = DataLoader(train_ds, batch_size=32, shuffle=True, pin_memory=True)
-val_loader = DataLoader(val_ds, batch_size=32, shuffle=False)
+test_ds = BreastCancerDataset(
+    img_dir=img_dir,
+    meta_data_csv_path=csv_file,
+    split=DatasetSplit.VAL,
+    num_images=(8, 16, 8, 16)
+)
+
+train_loader = DataLoader(train_ds, batch_size=8, shuffle=True, pin_memory=True)
+val_loader = DataLoader(val_ds, batch_size=8, shuffle=False)
+test_loader = DataLoader(test_ds, batch_size=1, shuffle=False)
 
 backbone = backbones.load("resnet50")
 net = SimpleNet(device)
@@ -37,20 +45,20 @@ net.load(
     input_shape=(3, 256, 256),
     pretrain_embed_dimension=512,
     target_embed_dimension=512,
-    patchsize=5,
+    patchsize=3,
     embedding_size=None,
-    meta_epochs=10,
+    meta_epochs=50,
     aed_meta_epochs=0,
-    gan_epochs=2,
+    gan_epochs=3,
     noise_std=0.05,
     dsc_layers=2,
-    dsc_hidden=64,
+    dsc_hidden=512,
     dsc_margin=0.85,
-    dsc_lr=0.001,
+    dsc_lr=0.0005,
     auto_noise=0,
     train_backbone=True,
     cos_lr=False,
-    lr=0.001,
+    lr=0.0005,
     pre_proj=0,
     proj_layer_type=0,
     mix_noise=1,
@@ -60,3 +68,5 @@ dataset_name = "rsna_breast_cancer"
 net.set_model_dir(models_dir, dataset_name)
 
 net.train(train_loader, val_loader)
+scores, segmentations, features, labels_gt, masks_gt = net.predict(val_loader)
+print(scores)
