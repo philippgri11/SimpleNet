6cfee0067d17ebce1f522f740bc7d4075ff4afc6
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-02-02 14:16:10 +0100

    device

--- wandb_training.py
+++ wandb_training.py
@@ -27,7 +27,7 @@ def pretrain_backbone(backbone, run, train_loader, val_loader, epochs=10, pos_im
     model = nn.Sequential(backbone, torch.nn.Linear(1000, 1, bias=False))
     model.to(device).train()
 
-    loss_fn = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([pos_images / len(train_loader)]))
+    loss_fn = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([pos_images / len(train_loader)]).to(device))
     optimizer = torch.optim.Adam(model.parameters(), 0.01)
     lr_lambda = lambda epoch: 0.9
     scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda)
