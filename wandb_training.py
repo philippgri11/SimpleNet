240f8ada5fe477849d3c6dba38e36243f950e6f9
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-03-13 11:00:45 +0100

    less images

--- wandb_training.py
+++ wandb_training.py
@@ -24,13 +24,13 @@ CANCER_CNT = 1158
 
 
 def train(config=None):
-    with wandb.init(config=config, group='DSC-Margin') as run:
+    with wandb.init(config=config, group='LessImages') as run:
         config = wandb.config
 
         train_ds = BreastCancerDataset(
             img_dir=img_dir,
             meta_data_csv_path=csv_file,
-            num_images=(8192, 0, 0, 0),
+            num_images=(1024, 0, 0, 0),
             resize=config.image_size[1:],
         )
 
@@ -38,7 +38,7 @@ def train(config=None):
             img_dir=img_dir,
             meta_data_csv_path=csv_file,
             split=DatasetSplit.VAL,
-            num_images=(1158, 8192, 1158, 0),
+            num_images=(1158, 1024, 1158, 0),
             resize=config.image_size[1:]
         )
 
