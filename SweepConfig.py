532530d8d526ef0e3bd1df3a277c6df53f9c9188
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-03-18 10:16:09 +0100

    WideResnet

--- SweepConfig.py
+++ SweepConfig.py
@@ -7,7 +7,7 @@ sweep_configuration = {
     'method': 'grid',
     'name': 'HyperparameterSearch' + datetime.now().strftime("%d/%m/%Y, %H:%M"),
     'parameters': {
-        'backbone': {'value': dataclasses.asdict(BackboneSetting('resnet50', ['layer1', 'layer2']))},
+        'backbone': {'value': dataclasses.asdict(BackboneSetting('wideresnet50', ['layer1', 'layer2']))},
         'batch_size': {'value': 16},
         'pretrain_embed_dimension': {'value': 768},
         'projection_dimension': {'value': 768},
@@ -16,18 +16,18 @@ sweep_configuration = {
         'meta_epochs': {'value': 50},
         'aed_meta_epochs': {'value': 5},  # used for cos_lr scheduler, but needs to be an int allways
         'gan_epochs': {'value': 5},
-        'noise_std': {'values': [0.015, 0.025]},
+        'noise_std': {'value': 0.03},
         'dsc_layers': {'value': 2},
         'dsc_hidden': {'value': 512},
-        'dsc_margin': {'values': [.3, .5]},
+        'dsc_margin': {'value': .3},
         'dsc_lr': {'value': 0.0002},  # LR for Discriminator
         'pre_proj_lr': {'value': 0.0001},
         'auto_noise': {'value': 0},  # scheint ein sinnloser Parameter zu sein
         'train_backbone': {'value': False},
         'cos_lr': {'value': False},
         'lr': {'value': 0.0001},  # LR for Projection and Backbone
-        'pre_proj': {'values': [1, 2]},  # Number of Layers for Projection
-        'proj_layer_type': {'values': [0, 999]},  # if > 1 then relu is added to all but the last layer of Projection
+        'pre_proj': {'value': 2},  # Number of Layers for Projection
+        'proj_layer_type': {'value': 999},  # if > 1 then relu is added to all but the last layer of Projection
         'mix_noise': {'value': 1},
     }
 }
--- wandb_training.py
+++ wandb_training.py
@@ -24,13 +24,13 @@ CANCER_CNT = 1158
 
 
 def train(config=None):
-    with wandb.init(config=config, group='LessImagesHyperparameter') as run:
+    with wandb.init(config=config, group='WideResNet') as run:
         config = wandb.config
 
         train_ds = BreastCancerDataset(
             img_dir=img_dir,
             meta_data_csv_path=csv_file,
-            num_images=(512, 0, 0, 0),
+            num_images=(2048, 0, 0, 0),
             resize=config.image_size[1:],
         )
 
