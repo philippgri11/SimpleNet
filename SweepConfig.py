7ce7260f36d26e5a0c223ee13194dba2f439604a
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-03-29 10:48:56 +0100

    sweep config

--- SweepConfig.py
+++ SweepConfig.py
@@ -13,23 +13,23 @@ sweep_configuration = {
         'projection_dimension': {'value': 1536},
         'image_size': {'value': (3, 224, 224)},
         'patch_size': {'value': 3},
-        'meta_epochs': {'value': 64},
+        'meta_epochs': {'value': 1000},
         'aed_meta_epochs': {'value': 5},  # used for cos_lr scheduler, but needs to be an int allways
-        'gan_epochs': {'value': 10},
+        'gan_epochs': {'value': 4},
         'noise_std': {'value': 0.015},
         'dsc_layers': {'value': 2},
         'dsc_hidden': {'value': 1024},
-        'dsc_margin': {'value': .5},
+        'dsc_margin': {'value': .4},
         'dsc_lr': {'value': 0.0002},  # LR for Discriminator
         'pre_proj_lr': {'value': 0.0001},
         'auto_noise': {'value': 0},  # scheint ein sinnloser Parameter zu sein
         'train_backbone': {'value': False},
-        'cos_lr': {'value': False},
+        'cos_lr': {'value': True},
         'lr': {'value': 0.0001},  # LR for Projection and Backbone
         'pre_proj': {'value': 1},  # Number of Layers for Projection
         'proj_layer_type': {'value': 0},  # if > 1 then relu is added to all but the last layer of Projection
         'mix_noise': {'value': 1},
-        'num_train_images': {'value': 1024},
+        'num_train_images': {'value': 40_000},
         'num_val_images': {'value': {'h': 64, 'c': 64}}
     }
 }
