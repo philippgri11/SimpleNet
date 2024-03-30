cfdf13561f1f1f37ebdfe6a14057fcb715e2c9d2
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-03-30 16:15:22 +0100

    config

--- SweepConfig.py
+++ SweepConfig.py
@@ -9,17 +9,17 @@ sweep_configuration = {
     'parameters': {
         'backbone': {'value': dataclasses.asdict(BackboneSetting('wideresnet50', ['layer2', 'layer3']))},
         'batch_size': {'value': 4},
-        'pretrain_embed_dimension': {'value': 1536},
-        'projection_dimension': {'value': 1536},
+        'pretrain_embed_dimension': {'value': 2048},
+        'projection_dimension': {'value': 2048},
         'image_size': {'value': (3, 224, 224)},
-        'patch_size': {'value': 3},
+        'patch_size': {'value': 5},
         'meta_epochs': {'value': 1000},
         'aed_meta_epochs': {'value': 5},  # used for cos_lr scheduler, but needs to be an int allways
         'gan_epochs': {'value': 4},
-        'noise_std': {'value': 0.015},
-        'dsc_layers': {'value': 2},
+        'noise_std': {'value': 0.005},
+        'dsc_layers': {'value': 3},
         'dsc_hidden': {'value': 1024},
-        'dsc_margin': {'value': .4},
+        'dsc_margin': {'value': .5},
         'dsc_lr': {'value': 0.0002},  # LR for Discriminator
         'pre_proj_lr': {'value': 0.0001},
         'auto_noise': {'value': 0},  # scheint ein sinnloser Parameter zu sein
@@ -27,9 +27,9 @@ sweep_configuration = {
         'cos_lr': {'value': True},
         'lr': {'value': 0.0001},  # LR for Projection and Backbone
         'pre_proj': {'value': 1},  # Number of Layers for Projection
-        'proj_layer_type': {'value': 0},  # if > 1 then relu is added to all but the last layer of Projection
+        'proj_layer_type': {'value': 999},  # if > 1 then relu is added to all but the last layer of Projection
         'mix_noise': {'value': 1},
         'num_train_images': {'value': 40_000},
-        'num_val_images': {'value': {'h': 64, 'c': 64}}
+        'num_val_images': {'value': {'h': 256, 'c': 256}}
     }
 }
